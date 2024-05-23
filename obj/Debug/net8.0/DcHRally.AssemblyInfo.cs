0bb34797bd623af80e1e349ae40ff5b2bbe5ca9e
Minikbusk <minikbusk@hotmail.com>
2024-05-23 11:21:58 +0200

    _

--- obj/Debug/net8.0/DcHRally.AssemblyInfo.cs
+++ obj/Debug/net8.0/DcHRally.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627b5ccdd69277c598e5e06507230a2d9962a297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178468ed03b14562206293f8eb7281792cdc3879")]
 [assembly: System.Reflection.AssemblyProductAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
