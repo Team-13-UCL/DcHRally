f714cdc70e4acc04dd5af01a043ca46baeb1abb0
Tobias Kjær <tobias.hougs.kjaer@gmail.com>
2024-05-26 18:39:00 +0200

    Problems with create track view and Track Builder court

--- obj/Debug/net8.0/DcHRally.AssemblyInfo.cs
+++ obj/Debug/net8.0/DcHRally.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8210cd79eaaa7d4504580028149748efe6c1655f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1e30237cb1be9145e2d8480f7e13616b2cf729")]
 [assembly: System.Reflection.AssemblyProductAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
