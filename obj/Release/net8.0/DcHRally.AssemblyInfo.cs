f2030e29c1191b02f7455093a699f4776f98dc70
Minikbusk <minikbusk@hotmail.com>
2024-05-27 10:15:28 +0200

    _

--- obj/Release/net8.0/DcHRally.AssemblyInfo.cs
+++ obj/Release/net8.0/DcHRally.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924f6694ea88a5b0154a1b69280d13e8ad74605f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb73ab45ae18cb7dd772c33780bde11da3568909")]
 [assembly: System.Reflection.AssemblyProductAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
