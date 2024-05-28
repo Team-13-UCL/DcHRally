77fb14216c16aa774d36eb165b30571431e6bdfd
Minikbusk <minikbusk@hotmail.com>
2024-05-28 10:35:07 +0200

    _

--- obj/Release/net8.0/DcHRally.AssemblyInfo.cs
+++ obj/Release/net8.0/DcHRally.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279c4aa52ee734434117e2cccc106bb5c2633a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f51ee7672062d6f0e93b62ea2550100c5d6988")]
 [assembly: System.Reflection.AssemblyProductAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DcHRally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
